fb680e4dedec789613e1efc0dcb80c202cebb860
mclarke16 <107970544+mclarke16@users.noreply.github.com>
2022-09-08 14:32:20 -0500

    First Commit

---
+++ makefile
@@ -0,0 +1,4 @@
+node.o:
+	gcc node.c -g -o rns
+clean:
+	rm -f *.o rns
---
+++ node.h
@@ -0,0 +1,29 @@
+#ifndef _node_h
+#define _node_h
+
+
+typedef struct Node
+{
+    int nodeID;
+    int conCount;
+    int *conList;
+}Node;
+
+// typedef struct Input
+// {
+//     char cmdarg = 'x';
+
+//     switch(cmdarg)
+//     {
+//     case '-h':
+//         printf("This is the help");
+//         break;
+    
+//     default:
+//         break;
+//     }
+
+// }input;
+
+
+#endif
